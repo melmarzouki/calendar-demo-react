c3f8f537a99c0b6f6cd5c98f4c238ad506002c5d
melmarzouki <melmarzouki17@gmail.com>
2021-04-04 13:06:59 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {useState} from 'react';
+import React from'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import {render} from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ReactCalendar = () => {
+    const [date, setDate] = useState(new Date());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const onChange = date => {
+        setDate(date);
+    }
+    return <div>
+        <Calendar onChange={onChange} value={date}/>
+        {date.toString()}
+        </div>;
+};
+
+render(<ReactCalendar/>, document.querySelector("#root"));
